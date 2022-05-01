cb3021ea7116d6ffe540bba1b3176afe69967442
macnaer <trofimchuk.an@gmail.com>
2022-05-01 13:09:22 +0300

    Add spa-client

--- ContactList.API/Program.cs
+++ ContactList.API/Program.cs
@@ -1,6 +1,7 @@
 using ContactList.Data.Data.Classes;
 using ContactList.Data.Data.Context;
 using ContactList.Data.Data.Intitializer;
+using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -18,6 +19,11 @@ builder.Services.AddDbContext<AppDbContext>();
 // Connect Interfecees to classes
 builder.Services.AddTransient<IContactListRepository, ContactListRepository>();
 
+// Connect SPA frontend 
+builder.Services.AddSpaStaticFiles(configuration => {
+    configuration.RootPath = "clientApp/build";
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -28,8 +34,18 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseAuthorization();
+app.UseSpaStaticFiles();
+
 
 app.MapControllers();
+
+app.UseSpa(spa => {
+    spa.Options.SourcePath = "clientApp";
+    if (builder.Environment.IsDevelopment())
+    {
+        spa.UseReactDevelopmentServer(npmScript: "start");
+    }
+});
 
 app.Run();
 
